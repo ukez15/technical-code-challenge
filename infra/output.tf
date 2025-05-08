2f7cfd7adb2754aa9fa173ebd8487ee37ae83cd4
Ukelabuchi Walson Ekpete <ukez15@gmail.com>
2025-05-08 20:11:21 +0100

    Update output.tf

--- infra/output.tf
+++ infra/output.tf
@@ -2,3 +2,17 @@ output "kube_config" {
   value = azurerm_kubernetes_cluster.aks.kube_config_raw
   sensitive = true
 }
+output "resource_group_name" {
+  description = "The name of the resource group"
+  value       = azurerm_resource_group.rg.name
+}
+
+output "aks_name" {
+  description = "The name of the AKS cluster"
+  value       = azurerm_kubernetes_cluster.aks.name
+}
+
+output "acr_login_server" {
+  description = "The login server of the ACR"
+  value       = azurerm_container_registry.acr.login_server
+}
