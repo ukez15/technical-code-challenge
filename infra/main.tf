620618684d7324622df80181afda5ce90d273646
Ukelabuchi Walson Ekpete <ukez15@gmail.com>
2025-05-08 19:24:20 +0100

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "=3.0.1"
     }
   }
@@ -9,6 +9,11 @@ terraform {
 
 provider "azurerm" {
   features {}
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
 
 resource "azurerm_resource_group" "rg" {
