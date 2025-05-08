0db09c0a900835ef278ab8bfd261ceead952bfd3
ukez15 <ukez15@gmail.com>
2025-05-08 23:08:28 +0100

    add multiple terraform imports

--- infra/main.tf
+++ infra/main.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "azurerm" {
   features {}
-  skip_provider_registration = true
+  resource_provider_registrations = "none"
 
   subscription_id = var.subscription_id
   client_id       = var.client_id
